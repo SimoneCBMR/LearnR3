20ebeb6f0d118d494deb4831cc88f5bec9162d05
Simone Jensen <simone.jensen@sund.ku.dk>
2023-04-25 15:43:00 +0200

    Automatically generated folder

---
+++ R/learning.R
@@ -0,0 +1,14 @@
+# Basics of R -------------------------------------------------------------
+
+colnames(airquality)
+str(airquality)
+summary(airquality)
+view(airquality)
+
+#use CTRL+shift+P and "style active file" to style fix your script
+
+# Loading packages --------------------------------------------------------
+
+library(tidyverse)
+
+
